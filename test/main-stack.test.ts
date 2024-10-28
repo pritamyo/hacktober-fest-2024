e5a153e5e9eafe6066e516526bd512401a97a6f6
Cristian Paniagua <tenondecrpc@gmail.com>
2024-10-27 23:57:08 -0300

    feat(project): Initial commit

---
+++ test/main-stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HacktoberFest2024 from '../lib/hacktober-fest-2024-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hacktober-fest-2024-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HacktoberFest2024.HacktoberFest2024Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
